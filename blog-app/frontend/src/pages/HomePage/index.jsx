d0ca1eba6b69362f6e80f47b0a0e703b4576e8de
WillHendo1 <williamhenderson1312@gmail.com>
2024-05-28 22:01:42 +0200

    day7

--- blog-app/frontend/src/pages/HomePage/index.jsx
+++ blog-app/frontend/src/pages/HomePage/index.jsx
@@ -4,20 +4,23 @@ import Navbar from "../../components/Navbar";
 import Heading from "../../components/Heading";
 import SubHeading from "../../components/SubHeading";
 import BlogGrid from "../../components/BlogGrid";
-import CategoryList from "../../components/CategoryList";
+import CategoriesList from "../../components/CategoryList";
 import Footer from "../../components/Footer";
 
 import blogService from "../../services/blogService";
+import categoryService from "../../services/categoryService";
 
 export default function Home() {
   const [blogs, setBlogs] = useState();
+  const [categories, setCategories] = useState();
 
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
         const blogsRes = await blogService.getBlogs();
-
+        const categoryRes = await categoryService.getCategories();
         setBlogs(blogsRes);
+        setCategories(categoryRes);
       } catch (err) {
         console.log(err);
       }
@@ -32,7 +35,7 @@ export default function Home() {
       <div className="container">
         <SubHeading subHeading={"Recent blog posts"} />
         <BlogGrid blogPosts={blogs} />
-        <CategoryList categories={[]} />
+        <CategoriesList categories={categories} />
         <Footer />
       </div>
     </>
